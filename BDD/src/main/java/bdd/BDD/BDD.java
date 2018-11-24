cc41a4e1a5d6ec2d21e751e9fd8b94d16c346e41
duckly <duckly_lee@163.com>
2018-11-24 20:34:34 +0800

    add classpath

---
+++ BDD/src/main/java/bdd/BDD/BDD.java
@@ -0,0 +1,47 @@
+package bdd.BDD;
+
+public interface BDD {
+    
+    // base functions
+    BDD var();
+    
+    BDD ithVar(int i);
+    
+    int varNum();
+    
+    BDD getOne();
+    
+    BDD getZero();
+    
+    // algebraic operations
+    BDD not();
+    
+    BDD and(BDD bdd);
+    
+    BDD or(BDD bdd);
+    
+    BDD xor(BDD bdd);
+    
+    // nonalgebraic operations
+    BDD restrict(BDD var, BDD bdd);
+    
+    BDD compose(BDD var, BDD bdd);
+    
+    BDD exists(BDD vars);
+    
+    BDD forall(BDD vars);
+    
+    BDD relprod(BDD vars, BDD bdd);
+    
+    // examining functions
+    boolean equals(BDD bdd);
+    
+    boolean eval(BDD assign);
+    
+    BDD satOne();
+    
+    Iterable<BDD> satAll();
+    
+    void free();
+    
+}
